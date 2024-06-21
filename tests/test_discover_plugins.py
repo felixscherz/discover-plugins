caa00b0910b9c0777e15fa4bd8cfbbf97cad24a5
Felix Scherz <felixwscherz@gmail.com>
2024-06-21 11:19:42 +0200

    test: streamline tests

--- tests/test_discover_plugins.py
+++ tests/test_discover_plugins.py
@@ -1,45 +1,19 @@
 import subprocess
 import sys
-from pathlib import Path
 
 import pytest
 
 
-def create_virtualenv(path: Path):
+@pytest.fixture
+def interpreter(tmp_path):
+    path = tmp_path / "venv"
     out = subprocess.run([sys.executable, "-m", "virtualenv", path.as_posix()])
+    assert out.returncode == 0
     return path / "bin/python"
 
-def test_install(tmp_path):
-    path = Path("./testdata/packages/myplugin")
-    venv_path = tmp_path / "venv"
-    interpreter = create_virtualenv(venv_path)
-    out = subprocess.run([interpreter, "-m", "pip", "install", "."])
-    with open("./discover_plugins.py") as handle:
-        code = handle.read()
-    out = subprocess.run([interpreter, "-c", code])
-    print(out)
 
-
-def test_install_and_find_code(tmp_path):
-    path = Path("./testdata/packages/myplugin")
-    venv_path = tmp_path / "venv"
-    interpreter = create_virtualenv(venv_path)
-    out = subprocess.run([interpreter, "-m", "pip", "install", "."])
-    with open("./discover_plugins.py") as handle:
-        code = handle.read()
-    out = subprocess.run([interpreter, "-c", code])
-    print(out)
-
-
-def test_install_and_find_code2(tmp_path):
-    path = Path("./testdata/packages/myplugin")
-    venv_path = tmp_path / "venv"
-    interpreter = create_virtualenv(venv_path)
-    out = subprocess.run([interpreter, "-m", "pip", "install", "."])
+def test_discover_plugins_for_specified_interpreter(interpreter):
+    out = subprocess.run([interpreter, "-m", "pip", "install", "./testdata/packages/myplugin"])
     out = subprocess.run(["discover-plugins", "--interpreter", interpreter])
-    print(out)
+    print(out.stdout)
     assert out.returncode == 0
-
-
-def test_find_code():
-    print(__file__)
